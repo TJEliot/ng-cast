48455e1326b8037a8d506f8b0a10200bffc81c8c
Lory Zhang <zhang.rouyun@gmail.com>
2017-12-15 14:28:35 -0800

    displayed dummy data

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,8 @@
 angular.module('video-player')
 
 .component('videoPlayer', {
-  // TODO
+  controller: function() {
+    this.video = exampleVideoData[0];
+  },
+  templateUrl: 'src/templates/videoPlayer.html'
 });
